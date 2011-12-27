de23830511092a71f471cffceb477a70b67c567f
Luiz Figueredo <llkinfo@yahoo.com.br>
2011-12-27 08:18:03 -0500

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signus::Application.config.secret_token = '384cb9a866684ff3eba913f753873237f70f8919d7e90e190a03074c150b00e8f96e3419f49cc2bbbd8f88ddf6287ea5cc707a7fdfa2e42c5f7dda54bfd7ae10'
